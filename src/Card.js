afd25c89a6c0d4abfd845ba989560442b1b944f5
konstsem <konstantin.semenov.2@gmail.com>
2019-02-15 17:14:12 +0300

    nothing

--- src/Card.js
+++ src/Card.js
@@ -13,15 +13,15 @@ export default class Card {
   init() {
     const div = document.createElement('div');
     div.classList.add('card');
-    const theme = document.createElement('h4');
+    const theme = document.createElement('p');
     theme.classList.add('theme');
     theme.textContent = this.theme;
     const sourceText = document.createElement('p');
-    sourceText.classList.add('sourceText');
+    sourceText.classList.add('sourceText', 'content');
     sourceText.textContent = this.sourceText;
     sourceText.hidden = false;
     const translation = document.createElement('p');
-    translation.classList.add('translation');
+    translation.classList.add('translation', 'content');
     translation.textContent = this.translation;
     translation.hidden = true;
     div.append(theme, sourceText, translation);
--- src/click.js
+++ src/click.js
@@ -1,9 +1,20 @@
 export default () => {
+  const translate = (div) => {
+    console.log(div);
+    // на это не смотрим, это ужасно
+    // div.children[1].hidden = true;
+    // div.children[2].hidden = false;
+    // setTimeout(() => {
+    //   console.log('bax');
+    //   div.children[1].hidden = false;
+    //   div.children[2].hidden = true;
+    // }, 1000);
+  };
   const cards = document.querySelectorAll('div.card');
   cards.forEach((card) => {
     card.addEventListener('click', (event) => {
       const currentCard = event.currentTarget;
-      console.log(currentCard);
+      translate(currentCard);
     });
   });
   cards.forEach((card) => {
